2640809fe9b1c03983928316819f336c999a7522
do350 <davidosborne3875@gmail.com>
2023-06-29 03:48:35 +0100

    Revert "Made Abstract Class for Character, Fixed Level Clipping"
    This reverts commit d54fe6630c398edebb38f3504958eb683be4df2b.

--- Assets/Scripts/Characters/Character.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Character
-{
-    public abstract void TakeDamage(float damage);
-    public abstract void Heal(float heal);
-}
